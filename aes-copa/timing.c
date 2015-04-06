00b9515f2c87fedf69d4dc72f6f794494fc979af
Brecht Wuyts <brecht.wuyts@gmail.com>
2015-04-06 12:06:56 +0200

    timing routine v2

--- aes-copa/timing.c
+++ aes-copa/timing.c
@@ -14,7 +14,7 @@ static inline unsigned int get_cyclecount (void)
 {
   unsigned int value;
   // Read CCNT Register
-  asm volatile ("MRC p15, 0, %0, c9, c13, 0\t\n": "=r"(value));  
+  asm volatile ("mrc p15, 0, %0, c9, c13, 0\t\n": "=r"(value));  
   return value;
 }
 
@@ -36,13 +36,13 @@ static inline void init_perfcounters (int32_t do_reset, int32_t enable_divider)
   value |= 16;
 
   // program the performance-counter control-register:
-  asm volatile ("MRC p15, 0, %0, c9, c12, 0\t\n" :: "r"(value));  
+  asm volatile ("mcr p15, 0, %0, c9, c12, 0\t\n" :: "r"(value));  
 
   // enable all counters:  
-  asm volatile ("MRC p15, 0, %0, c9, c12, 1\t\n" :: "r"(0x8000000f));  
+  asm volatile ("mcr p15, 0, %0, c9, c12, 1\t\n" :: "r"(0x8000000f));  
 
   // clear overflows:
-  asm volatile ("MRC p15, 0, %0, c9, c12, 3\t\n" :: "r"(0x8000000f));
+  asm volatile ("mcr p15, 0, %0, c9, c12, 3\t\n" :: "r"(0x8000000f));
 }
 
 int main() {
@@ -52,39 +52,38 @@ unsigned char nonce[16] = {0x09,0xf9,0x11,0x02,0x9d,0x74,0xe3,0x5b,0xd8,0x41,0x5
 unsigned char in[16*BLOCKS*100], out[16*BLOCKS*100], tag[16];
 
   int i, j, k, l=100;
-  asm ("MRC p15, 0, %0, C9, C14, 0\n\t" :: "r"(1));
 
 //  srand(time(NULL));
   for (i=0; i<16*BLOCKS*l; i++)
     in[i] = (unsigned char)rand();
 
    init_perfcounters(1,0);
-// /*
-//    unsigned int overhead = get_cyclecount();
-//    overhead = get_cyclecount() - overhead;   
-//   
-//   
-//  uint32_t t0,t1;
-//   
-//  uint32_t tMin = 0xFFFFFFFF;         /* big number to start */
-//   
-//   printf("Cycles for calibrate: %d\n", overhead);
-// 
-//   for (j=0;j<1000;j++) 
-//       crypto_aead_encrypt(out,tag,in,16*BLOCKS*100,0,0,0,nonce,key);
-// 
-// 
-//   for (k=0;k < TIMER_SAMPLE_CNT;k++) {
-//     t0 = get_cyclecount();
-//       crypto_aead_encrypt(out,tag,in,16*BLOCKS*100,0,0,0,nonce,key);
-// 
-//     t1 = get_cyclecount();
-//     if (tMin > t1-t0 - overhead) tMin = t1-t0 - overhead;
-//   }
-//   printf("Cycles for YAES: %d\n", tMin);
-//   printf("Cycles per byte: %f\n", tMin/(16.0*BLOCKS*l));
-//   
-// 
-//   return 0;*/	
+
+   unsigned int overhead = get_cyclecount();
+   overhead = get_cyclecount() - overhead;   
+  
+  
+ uint32_t t0,t1;
+  
+ uint32_t tMin = 0xFFFFFFFF;         /* big number to start */
+  
+  printf("Cycles for calibrate: %d\n", overhead);
+
+  for (j=0;j<1000;j++) 
+      crypto_aead_encrypt(out,tag,in,16*BLOCKS*100,0,0,0,nonce,key);
+
+
+  for (k=0;k < TIMER_SAMPLE_CNT;k++) {
+    t0 = get_cyclecount();
+      crypto_aead_encrypt(out,tag,in,16*BLOCKS*100,0,0,0,nonce,key);
+
+    t1 = get_cyclecount();
+    if (tMin > t1-t0 - overhead) tMin = t1-t0 - overhead;
+  }
+  printf("Cycles for YAES: %d\n", tMin);
+  printf("Cycles per byte: %f\n", tMin/(16.0*BLOCKS*l));
+  
+
+  return 0;
 }
 
