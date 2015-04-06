0ed9e344630cae0930b547627ede531973bf1c5e
Brecht Wuyts <brecht.wuyts@gmail.com>
2015-04-06 10:20:29 +0200

    timing routine v2

--- aes-copa/timing.c
+++ aes-copa/timing.c
@@ -4,85 +4,12 @@
 #include <stdlib.h>
 #include <time.h>
 
-//#include <x86intrin.h>
 #include "encrypt.h"
 
 #define BLOCKS 7
-// #define HIRES
-// 
-// #ifdef HIRES
-// /************** Timing routine (for performance measurements) ***********/
-// /* By Doug Whiting */
-// /* unfortunately, this is generally assembly code and not very portable */
-// #if defined(_M_IX86) || defined(__i386) || defined(_i386) || defined(__i386__) || defined(i386) || \
-//     defined(_X86_)   || defined(__x86_64__) || defined(_M_X64) || defined(__x86_64)
-// #define _Is_X86_    1
-// #endif
-// 
-// #if  defined(_Is_X86_) && (!defined(__STRICT_ANSI__)) && (defined(__GNUC__) || !defined(__STDC__)) && \
-//     (defined(__BORLANDC__) || defined(_MSC_VER) || defined(__MINGW_H) || defined(__GNUC__))
-// #define HI_RES_CLK_OK         1          /* it's ok to use RDTSC opcode */
-// 
-// #if defined(_MSC_VER) // && defined(_M_X64)
-// #include <intrin.h>
-// #pragma intrinsic(__rdtsc)         /* use MSVC rdtsc call where defined */
-// #endif
-// 
-// #endif
-// 
-// uint32_t HiResTime(void)           /* return the current value of time stamp counter */
-//     {
-// #if defined(HI_RES_CLK_OK)
-//     uint32_t x[2];
-// #if   defined(__BORLANDC__)
-// #define COMPILER_ID "BCC"
-//     __emit__(0x0F,0x31);           /* RDTSC instruction */
-//     _asm { mov x[0],eax };
-// #elif defined(_MSC_VER)
-// #define COMPILER_ID "MSC"
-// #if defined(_MSC_VER) // && defined(_M_X64)
-// #else    x[0] = (uint32_t) __rdtsc();
-// 
-//     _asm { _emit 0fh }; _asm { _emit 031h };
-//     _asm { mov x[0],eax };
-// #endif
-// #elif defined(__MINGW_H) || defined(__GNUC__)
-// #define COMPILER_ID "GCC"
-//     asm volatile("rdtsc" : "=a"(x[0]), "=d"(x[1]));
-// #else
-// #error  "HI_RES_CLK_OK -- but no assembler code for this platform (?)"
-// #endif
-//     return x[0];
-// #else
-//     /* avoid annoying MSVC 9.0 compiler warning #4720 in ANSI mode! */
-// #if (!defined(_MSC_VER)) || (!defined(__STDC__)) || (_MSC_VER < 1300)
-//     FatalError("No support for RDTSC on this CPU platform\n");
-// #endif
-//     return 0;
-// #endif /* defined(HI_RES_CLK_OK) */
-//     }
-// 
-// #define ALIGN __attribute__ ((aligned(32)))    
-// 
+
  #define TIMER_SAMPLE_CNT (10000)
-// 
-// uint32_t calibrate()
-// {
-//     uint32_t dtMin = 0xFFFFFFFF;        /* big number to start */
-//     uint32_t t0,t1,i;
-// 
-//     for (i=0;i < TIMER_SAMPLE_CNT;i++)  /* calibrate the overhead for measuring time */
-//         {
-//         t0 = HiResTime();
-//         t1 = HiResTime();
-//         if (dtMin > t1-t0)              /* keep only the minimum time */
-//             dtMin = t1-t0;
-//         }
-//     return dtMin;
-// }
-// #else
-// #define ALIGN
-// #endif
+
 static inline unsigned int get_cyclecount (void)
 {
   unsigned int value;
@@ -130,39 +57,32 @@ unsigned char in[16*BLOCKS*100], out[16*BLOCKS*100], tag[16];
   for (i=0; i<16*BLOCKS*l; i++)
     in[i] = (unsigned char)rand();
 
+   init_perfcounters(1,0);
+
+   unsigned int overhead = get_cyclecount();
+   overhead = get_cyclecount() - overhead;   
   
-//    unsigned int overhead = get_cyclecount();
-//    overhead = get_cyclecount() - overhead;   
   
-// #ifdef HIRES
+ uint32_t t0,t1;
   
-//  init_perfcounters(1,0);
-//  uint32_t t0,t1;
+ uint32_t tMin = 0xFFFFFFFF;         /* big number to start */
   
-//  uint32_t tMin = 0xFFFFFFFF;         /* big number to start */
+  printf("Cycles for calibrate: %d\n", overhead);
+
+  for (j=0;j<1000;j++) 
+      crypto_aead_encrypt(out,tag,in,16*BLOCKS*100,0,0,0,nonce,key);
+
+
+  for (k=0;k < TIMER_SAMPLE_CNT;k++) {
+    t0 = get_cyclecount();
+      crypto_aead_encrypt(out,tag,in,16*BLOCKS*100,0,0,0,nonce,key);
+
+    t1 = get_cyclecount();
+    if (tMin > t1-t0 - overhead) tMin = t1-t0 - overhead;
+  }
+  printf("Cycles for YAES: %d\n", tMin);
+  printf("Cycles per byte: %f\n", tMin/(16.0*BLOCKS*l));
   
-//   printf("Cycles for calibrate: %d\n", overhead);
-// 
-//   for (j=0;j<1000;j++) 
-//       crypto_aead_encrypt(out,tag,in,16*BLOCKS*100,0,0,0,nonce,key);
-// 
-// 
-//   for (k=0;k < TIMER_SAMPLE_CNT;k++) {
-//     t0 = get_cyclecount();
-// // #endif
-//       crypto_aead_encrypt(out,tag,in,16*BLOCKS*100,0,0,0,nonce,key);
-// 
-// // #ifdef HIRES
-//     t1 = get_cyclecount();
-//     if (tMin > t1-t0 - overhead) tMin = t1-t0 - overhead;
-//   }
-// // #endif
-//   
-// // #ifdef HIRES
-//   printf("Cycles for YAES: %d\n", tMin);
-//   printf("Cycles per byte: %f\n", tMin/(16.0*BLOCKS*l));
-// // #endif
-//   
 
   return 0;
 }
