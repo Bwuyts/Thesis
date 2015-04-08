e924695431bfe6ec0034c41dc143412fbeec1bd5
Brecht Wuyts <brecht.wuyts@gmail.com>
2015-04-08 14:02:53 +0200

    test 32

--- aes-copa/timing.c
+++ aes-copa/timing.c
@@ -4,12 +4,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <linux/kernel.h>
- #include <linux/module.h>
-// #include <linux/init.h>
-// #include <linux/moduleparam.h>
-// #include <linux/workqueue.h>
-// #include <linux/time.h>
-// #include <asm/mutex.h>
+#include <linux/module.h>
+
 
 #include "encrypt.h"
 
