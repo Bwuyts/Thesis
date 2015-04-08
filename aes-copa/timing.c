5c302410cfd77af254ec02877dd97dddb80cf71f
Brecht Wuyts <brecht.wuyts@gmail.com>
2015-04-08 16:17:47 +0200

    test33

--- aes-copa/timing.c
+++ aes-copa/timing.c
@@ -4,7 +4,67 @@
 #include <stdlib.h>
 #include <time.h>
 #include <linux/kernel.h>
-//#include <linux/module.h>
+#include <linux/module.h>
+#include <linux/smp.h>
+/** -- Configuration stuff ------------------------------------------------- */
+
+#define DRVR_NAME "enable_arm_pmu"
+
+#if !defined(__arm__)
+#error Module can only be compiled on ARM machines.
+/** -- Initialization & boilerplate ---------------------------------------- */
+
+#define PERF_DEF_OPTS (1 | 16)
+#define PERF_OPT_RESET_CYCLES (2 | 4)
+#define PERF_OPT_DIV64 (8)
+
+static void
+enable_cpu_counters(void* data)
+{
+        printk(KERN_INFO "[" DRVR_NAME "] enabling user-mode PMU access on CPU #%d",
+                smp_processor_id());
+
+        /* Enable user-mode access to counters. */
+        asm volatile("mcr p15, 0, %0, c9, c14, 0" :: "r"(1));
+        /* Program PMU and enable all counters */
+        asm volatile("mcr p15, 0, %0, c9, c12, 0" :: "r"(PERF_DEF_OPTS));
+        asm volatile("mcr p15, 0, %0, c9, c12, 1" :: "r"(0x8000000f));
+}
+
+static void
+disable_cpu_counters(void* data)
+{
+        printk(KERN_INFO "[" DRVR_NAME "] disabling user-mode PMU access on CPU #%d",
+                smp_processor_id());
+
+        /* Program PMU and disable all counters */
+        asm volatile("mcr p15, 0, %0, c9, c12, 0" :: "r"(0));
+        asm volatile("mcr p15, 0, %0, c9, c12, 2" :: "r"(0x8000000f));
+        /* Disable user-mode access to counters. */
+        asm volatile("mcr p15, 0, %0, c9, c14, 0" :: "r"(0));
+}
+
+static int __init
+init(void)
+{
+        on_each_cpu(enable_cpu_counters, NULL, 1);
+        printk(KERN_INFO "[" DRVR_NAME "] initialized");
+        return 0;
+}
+
+static void __exit
+fini(void)
+{
+        on_each_cpu(disable_cpu_counters, NULL, 1);
+        printk(KERN_INFO "[" DRVR_NAME "] unloaded");
+}
+
+MODULE_AUTHOR("Austin Seipp <aseipp@pobox.com>");
+MODULE_LICENSE("Dual MIT/GPL");
+MODULE_DESCRIPTION("Enables user-mode access to ARMv7 PMU counters");
+MODULE_VERSION("0:0.1-dev");
+module_init(init);
+module_exit(fini);
 
 
 #include "encrypt.h"
@@ -15,72 +75,7 @@
 
 
  #define TIMER_SAMPLE_CNT (10000)
-static void
-enable_cpu_counters(void* data)
-{
-        /* Enable user-mode access to counters. */
-        asm volatile("mcr p15, 0, %0, c9, c14, 0" :: "r"(1));
-        /* Program PMU and enable all counters */
-        asm volatile("mcr p15, 0, %0, c9, c12, 0" :: "r"(PERF_DEF_OPTS));
-        asm volatile("mcr p15, 0, %0, c9, c12, 1" :: "r"(0x8000000f));
-}
- 
 
-static int __init__ init(void)
-{
-        on_each_cpu(enable_cpu_counters, NULL, 1);
-        printk(KERN_INFO "[" DRVR_NAME "] initialized");
-        return 0;
-}
-
-static void __exit__ fini(void)
-{
-        on_each_cpu(disable_cpu_counters, NULL, 1);
-        printk(KERN_INFO "[" DRVR_NAME "] unloaded");
-}
-
-// static inline void init_perfcounters (int32_t do_reset, int32_t enable_divider)
-// {
-// //   /* enable user-mode access to the performance counter*/
-// //   asm ("mcr p15, 0, %0, C9, C14, 0\n\t" :: "r"(1));
-// // 
-// //   /* disable counter overflow interrupts (just in case)*/
-// //   asm ("mcr p15, 0, %0, C9, C14, 2\n\t" :: "r"(0x8000000f));
-//   int32_t value = 1;
-// 
-//   // peform reset:  
-//   if (do_reset)
-//   {
-//     value |= 2;     // reset all counters to zero.
-//     value |= 4;     // reset cycle counter to zero.
-//   } 
-// 
-//   if (enable_divider)
-//     value |= 8;     // enable "by 64" divider for CCNT.
-// 
-//   value |= 16;
-// 
-//   // program the performance-counter control-register:
-//   asm volatile ("mcr p15, 0, %0, c9, c12, 0\t\n" :: "r"(value));  
-// 
-//   // enable all counters:  
-//   asm volatile ("mcr p15, 0, %0, c9, c12, 1\t\n" :: "r"(0x8000000f));  
-// 
-//   // clear overflows:
-//   asm volatile ("mcr p15, 0, %0, c9, c12, 3\t\n" :: "r"(0x8000000f));
-// }
-
-static inline uint32_t
-rdtsc32(void)
-{
-//#if defined(__GNUC__) && defined(__ARM_ARCH_7A__)
-        uint32_t r = 0;
-        asm volatile("mrc p15, 0, %0, c9, c13, 0" : "=r"(r) );
-        return r;
-//#else
-//#error Unsupported architecture/compiler!
-//#endif
-}
 
 
 int main() {
@@ -128,3 +123,7 @@ unsigned long long tag[16];
   return 0;
 }
 
+
+
+module_init(init);
+module_exit(fini);
