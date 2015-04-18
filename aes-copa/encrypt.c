93b3c2bcfeebefb2d3c555a01f6d335d15f962c4
Brecht Wuyts <brecht.wuyts@gmail.com>
2015-04-18 16:44:07 +0200

    gf7

--- aes-copa/encrypt.c
+++ aes-copa/encrypt.c
@@ -146,6 +146,7 @@ static inline void gf128_mul7(block_t res, const block_t x)
 {
 	block_t x2, x4;
 	uint32_t X0, X1,X2,X3;
+	uint32_t X0_2, X1_2,X2_2,X3_2;
 	uint32_t* xp =(uint32_t*) x;
 	uint32_t* x2p =(uint32_t*) x2;
 	uint32_t* x4p =(uint32_t*) x4;
@@ -155,17 +156,17 @@ static inline void gf128_mul7(block_t res, const block_t x)
 	X1 = GETU32(x+4);
 	X2 = GETU32(x+8);
 	X3 = GETU32(x+12);
-	X0 = (X0<<1) | (X1>>31);
-	X1 = (X1<<1) | (X2>>31);
-	X2 = (X2<<1) | (X3>>31);
-	X3 = (X3<<1) ;
+	X0_2 = (X0<<1) | (X1>>31);
+	X1_2 = (X1<<1) | (X2>>31);
+	X2_2 = (X2<<1) | (X3>>31);
+	X3_2 = (X3<<1) ;
 	if (msb) {
-		X3 ^= 0x87;
+		X3_2 ^= 0x87;
 	}
-	PUTU32(x2, X0);
-	PUTU32(x2+4, X1);
-	PUTU32(x2+8, X2);
-	PUTU32(x2+12, X3);
+	PUTU32(x2, X0_2);
+	PUTU32(x2+4, X1_2);
+	PUTU32(x2+8, X2_2);
+	PUTU32(x2+12, X3_2);
 	
 	msb = x2[0] & 0x80;
 	shl_block(x4, x2);
