580955ef580a0bbe00a2ca8277317f6ad55eeb9f
Brecht Wuyts <brecht.wuyts@gmail.com>
2015-04-18 10:06:24 +0200

    AES modified

--- aes-copa/aes-core.c
+++ aes-copa/aes-core.c
@@ -225,8 +225,11 @@ void aes_first_addroundkey(uint8_t state[], const uint8_t text[], const uint8_t 
 	uint32_t* stateP = (uint32_t*) state;
 	uint32_t* textP = (uint32_t*) text;
 	uint32_t* round_keyP = (uint32_t*) round_key;
-	for (i = 0; i < 4; ++i)
-		stateP[i] = textP[i] ^ round_keyP[i];
+
+		stateP[0] = textP[0] ^ round_keyP[0];
+		stateP[1] = textP[1] ^ round_keyP[1];
+		stateP[2] = textP[2] ^ round_keyP[2];
+		stateP[3] = textP[3] ^ round_keyP[3];
 }
 
 void aes_addroundkey(uint8_t state[], const uint8_t round_key[])
@@ -234,8 +237,11 @@ void aes_addroundkey(uint8_t state[], const uint8_t round_key[])
 	unsigned int i;
 	uint32_t* stateP = (uint32_t*) state;
 	uint32_t* round_keyP = (uint32_t*) round_key;
-	for (i = 0; i < 4; ++i)
-		stateP[i] ^= round_keyP[i];
+
+		stateP[0] ^= round_keyP[0];
+		stateP[1] ^= round_keyP[1];
+		stateP[2] ^= round_keyP[2];
+		stateP[3] ^= round_keyP[3];
 }
 
 void aes_last_addroundkey(uint8_t text[], const uint8_t state[], const uint8_t round_key[])
@@ -244,8 +250,10 @@ void aes_last_addroundkey(uint8_t text[], const uint8_t state[], const uint8_t r
 	uint32_t* stateP = (uint32_t*) state;
 	uint32_t* textP = (uint32_t*) text;
 	uint32_t* round_keyP = (uint32_t*) round_key;
-	for (i = 0; i < 4; ++i)
-		textP[i] = stateP[i] ^ round_keyP[i];
+		textP[0] = stateP[0] ^ round_keyP[0];
+		textP[1] = stateP[1] ^ round_keyP[1];
+		textP[2] = stateP[2] ^ round_keyP[2];
+		textP[3] = stateP[3] ^ round_keyP[3];
 }
 
 void aes_subbytes_shiftrows(uint8_t state[])
