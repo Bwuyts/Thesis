7d3bfbd818ceb6d877367c68f272ebc68855d62b
Brecht Wuyts <brecht.wuyts@gmail.com>
2015-04-28 20:17:41 +0200

    GFtimetest

--- AES/timing2.c
+++ AES/timing2.c
@@ -184,10 +184,10 @@ unsigned char out[16];
      
       for (k=0;k < TIMER_SAMPLE_CNT;k++) {
                 t0 = rdtsc32();
-            gf128_mul2(out, key);
-
-                    t1 = rdtsc32();
-                if (tMin > t1-t0 - overhead) tMin = t1-t0 - overhead;
+                for(i=0;i<1000;i++)
+                    gf128_mul2(out, key);
+                t1 = rdtsc32();
+        if (tMin > t1-t0 - overhead) tMin = t1-t0 - overhead;
   }
     
       printf("GF2: %d\n", tMin);
